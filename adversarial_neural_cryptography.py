cc7131d52d154581f15035c4979bca8bc952a757
andyp <andyp@localhost.localdomain>
2017-03-30 00:30:18 -0400

    static msg and key to create lookup table

--- adversarial_neural_cryptography.py
+++ adversarial_neural_cryptography.py
@@ -1,48 +1,44 @@
 # -*- coding: utf-8 -*-
 """
 Created on Tue Nov  1 09:52:44 2016
-
+Modified on Fri Mar 24
 @author: liam schoneveld
-
+@author: Andy Pitcher
 Implementation of model described in 'Learning to Protect Communications with 
 Adversarial Neural Cryptography' (Martín Abadi & David G. Andersen, 2016, 
 https://arxiv.org/abs/1610.06918)
 """
-
+from termcolor import colored
 import theano
+import texttable as tt
+import re
 import theano.tensor as T
 import numpy as np
 import matplotlib.pyplot as plt
 from layers import ConvLayer, HiddenLayer, get_all_params
 from lasagne.updates import adam
 
+#Banner
+Banner= '''
+
+ ___  _  _  ___  ___   __  _  _   __        ___             
+|_ _|| \\| |/ __|| __| / / / |/ | /  \\  ___ | _ \ _ _  ___  (_) ___  __ | |
+ | | | .` |\\__ \\| _| \/ _ \\| || || () ||___||  _/| '_|/ _ \ | |/ -_)/ _||  _|
+|___||_|\\_||___/|___|\___/|_||_| \\__/	   |_|  |_|  \___/_/ |\\___|\\__| \\__|
+                                                         |__/
+
+'''
+result = string.replace("\\","")
 # Parameters
-batch_size = 512
-msg_len = 16
-key_len = 16
-comm_len = 16
+batch_size = 1
+msg_len = 8
+key_len = 8
+comm_len = 8
 
 # Set this flag to exclude convolutional layers from the networks
 skip_conv = False
 
-# Function to generate n random messages and keys
-def gen_data(n=batch_size, msg_len=msg_len, key_len=key_len):
-    return (np.random.randint(0, 2, size=(n, msg_len))*2-1).\
-                astype(theano.config.floatX),\
-           (np.random.randint(0, 2, size=(n, key_len))*2-1).\
-                astype(theano.config.floatX)
-
-# Function to assess a batch by eye (see what the errors look like)
-def assess(pred_fn, n=batch_size, msg_len=msg_len, key_len=key_len):
-    msg_in_val, key_val = gen_data(n, msg_len, key_len)
-    return np.round(np.abs(msg_in_val[0:n] - \
-           pred_fn(msg_in_val[0:n], key_val[0:n])), 0)
-
-# Function to get the error over just one batch
-def err_over_samples(err_fn, n=batch_size):
-    msg_in_val, key_val = gen_data(n)
-    return err_fn(msg_in_val[0:n], key_val[0:n])
-
+#stores all the parameters and tensors relavent to all of the convolutional layers in the model
 class StandardConvSetup():
     '''
     Standard convolutional layers setup used by Alice, Bob and Eve.
@@ -50,7 +46,6 @@ class StandardConvSetup():
     Output is 4d tensor of shape (batch_size, 1, msg_len, 1)
     '''
     def __init__(self, reshaped_input, name='unnamed'):
-        
         self.name = name
         self.conv_layer1 = ConvLayer(reshaped_input,
                                      filter_shape=(2, 1, 4, 1), #num outs, num ins, size
@@ -118,7 +113,6 @@ else:
     alice_conv = StandardConvSetup(alice_conv_in, 'alice')
     # Get the output communication
     alice_comm = alice_conv.output.reshape((batch_size, msg_len))
-
 # Bob's input is the concatenation of Alice's communication and the key
 bob_in = T.concatenate([alice_comm, key], axis=1)
 # He decrypts using a hidden layer and a conv net as per Alice
@@ -174,6 +168,8 @@ decrypt_err_bob = T.mean(T.abs_(msg_in - bob_msg))
 # --> Bob wants Eve to do only as good as random guessing
 loss_bob = decrypt_err_bob + (1. - decrypt_err_eve) ** 2.
 
+##Variable 'cipher' returns the value of Alice output: 'msg_in:key'->'cipher'
+cipher = alice_comm
 
 # Get all the parameters for Bob and Alice, make updates, train and pred funcs
 params   = {'bob' : get_all_params([bob_conv, bob_hid, 
@@ -181,6 +177,10 @@ params   = {'bob' : get_all_params([bob_conv, bob_hid,
 updates  = {'bob' : adam(loss_bob, params['bob'])}
 err_fn   = {'bob' : theano.function(inputs=[msg_in, key],
                                     outputs=decrypt_err_bob)}
+##This fn takes as input the 'msg_val' and 'key_val' created in each iterations from gen_st_data()
+cipher_fn   = {'bob' : theano.function(inputs=[msg_in, key],
+                                    outputs=cipher)}
+
 train_fn = {'bob' : theano.function(inputs=[msg_in, key],
                                     outputs=loss_bob,
                                     updates=updates['bob'])}
@@ -196,46 +196,90 @@ train_fn['eve'] = theano.function(inputs=[msg_in, key],
                                   updates=updates['eve'])
 pred_fn['eve']  = theano.function(inputs=[msg_in, key], outputs=eve_msg)
 
+# Generate static matrix: integer -> binary -> replace 0 with -1 -> create the matrix 
+def gen_st_data(integer,n=msg_len):
+        #Convert 'integer' to bin
+	d=bin(integer)[2:].zfill(n)
+	#Format the binary 'd' and replace 0's with -1's to match the layers matrix format
+        a = (" ".join(d))
+        b = a.replace('0', '-1') 
+        #Generate the matrix out ot the binary
+	gen_matrix= np.matrix(b)
+        return(gen_matrix)
+
+
 # Function for training either Bob+Alice or Eve for some time
-def train(bob_or_eve, results, max_iters, print_every, es=0., es_limit=100):
+def train(integer,key,bob_or_eve,results, max_iters, print_every, es=0., es_limit=10):
     count = 0
     for i in range(max_iters):
-        # Generate some data
-        msg_in_val, key_val = gen_data()
-        # Train on this batch and get loss
-        loss = train_fn[bob_or_eve](msg_in_val, key_val)
-        # Store absolute decryption error of the model on this batch
-        results = np.hstack((results, 
+        	# Generate some data
+        	#Feed 'key_val' and 'msg_in_val' from gen_st_data()
+		key_val = gen_st_data(key)
+        	msg_in_val = gen_st_data(integer)
+		# Train on this batch and get loss
+        	loss = train_fn[bob_or_eve](msg_in_val, key_val)
+		#Retrieve the 'cipher' value from given 'msg_in_val:key_val'
+		cipher_val = cipher_fn[bob_or_eve](msg_in_val, key_val)
+		#Setting entries > 0 to 1 and entries <=0 to 0
+		cipher_val[cipher_val <= 0] = '0'
+		cipher_val[cipher_val > 0] = '1'
+		#Create 'result_ciph' containing a valid binary format eg. 0001001
+		cipher_val=(re.sub('[\[\]]', '', np.array_str(cipher_val))).replace(".", "")
+		result_ciph=cipher_val.replace(" ", "")
+        	# Store absolute decryption error of the model on this batch
+		results = np.hstack((results, 
                              err_fn[bob_or_eve](msg_in_val, key_val).sum()))
-        # Print loss now and then
-        if i % print_every == 0:
-            print 'training loss:', loss
-        # Early stopping if we see a low-enough decryption error enough times
-        if es and loss < es:
-            count += 1
-            if count > es_limit:
-                break
-    return np.hstack((results, np.repeat(results[-1], max_iters - i - 1)))
+        	# Print loss now and then
+        	if i % print_every == 0:
+			count +=1
+			count -=1
+        	# Early stopping if we see a low-enough decryption error enough times
+        	if es and loss < es:
+            		count += 1
+            	if count > es_limit:
+                	break
+    return (result_ciph, np.hstack((results, np.repeat(results[-1], max_iters - i - 1))))
 
-# Initialise some empty results arrays
-results_bob, results_eve = [], []
-adversarial_iterations = 60
+###############################################################################################################################
+# Initialise the values for iterations, message from 0 to 'last_message', static 'key' value with its binary version 'key_bin'#
+###############################################################################################################################
 
-# Perform adversarial training
-for i in range(adversarial_iterations):
-    n = 2000
-    print_every = 100
-    print 'training bob and alice, run:', i+1
-    results_bob = train('bob', results_bob, n, print_every, es=0.01)
-    print 'training eve, run:', i+1
-    results_eve = train('eve', results_eve, n, print_every, es=0.01)
+adversarial_iterations = 1
+last_message= 256
+key=112
+key_bin=bin(key)[2:].zfill(msg_len)
 
-# Plot the results
-plt.plot([np.min(results_bob[i:i+n]) for i in np.arange(0, 
-          len(results_bob), n)])
-plt.plot([np.min(results_eve[i:i+n]) for i in np.arange(0, 
-          len(results_eve), n)])
-plt.legend(['bob', 'eve'])
-plt.xlabel('adversarial iteration')
-plt.ylabel('lowest decryption error achieved')
-plt.show()
+
+#Initialise simple table for storing the final lookup table
+tab = tt.Texttable()
+x = [[]] 
+
+
+print "\t\t\n\n\n\nLookup table for static KEY -> [",key_bin,"] (",key,")"
+
+#Loop from 0 to 'last_message'
+
+for message in range(last_message):
+	#Initialise a new 'results_bob' array for each 'message'
+	results_bob=[]
+      	message_bin=bin(message)[2:].zfill(msg_len)
+	# Perform adversarial training
+	for i in range(adversarial_iterations):
+		#Set 'n' for the number of 'max_iters'
+    		n = 1
+    		print_every = 100
+    		cipher,results_bob = train(message,key, 'bob', results_bob, n, print_every, es=0.3)
+		x.append([message,message_bin,cipher])
+
+#Print the table with values
+tab.add_rows(x)
+tab.set_cols_align(['r','r','r'])
+tab.header(['Message_int', 'Message(8bits)', 'Cipher(8bits)'])
+print tab.draw()
+
+
+ ___  _  _  ___  ___   __  _  _   __        ___             _           _   
+|_ _|| \| |/ __|| __| / / / |/ | /  \  ___ | _ \ _ _  ___  (_) ___  __ | |_ 
+ | | | .` |\__ \| _| / _ \| || || () ||___||  _/| '_|/ _ \ | |/ -_)/ _||  _|
+|___||_|\_||___/|___|\___/|_||_| \__/      |_|  |_|  \___/_/ |\___|\__| \__|
+                                                         |__/               
